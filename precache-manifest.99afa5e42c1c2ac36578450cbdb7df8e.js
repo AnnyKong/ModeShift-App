f0186819f188a60edf10e365ed67f07810486cf8
Anny Kong <yk57@cs.washington.edu>
2019-05-01 00:15:26 -0700

    Updates

---
+++ precache-manifest.99afa5e42c1c2ac36578450cbdb7df8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbba4ce7a9a365dcdae7e1976e661386",
+    "url": "/ModeShift/index.html"
+  },
+  {
+    "revision": "e6f202d5d62de48ce196",
+    "url": "/ModeShift/static/css/main.07b611b6.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ModeShift/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e6f202d5d62de48ce196",
+    "url": "/ModeShift/static/js/main.455e4aa7.chunk.js"
+  },
+  {
+    "revision": "e2151f4c4075cdc8cc3e",
+    "url": "/ModeShift/static/js/runtime~main.d93cac30.js"
+  }
+]);
