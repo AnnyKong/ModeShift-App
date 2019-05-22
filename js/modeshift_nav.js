6a58e7a222e3d13892d7ada39aa6a60133eb4301
Anny(Yanmeng Kong) <yk57@cs.washington.edu>
2019-05-22 04:17:26 -0700

    Updates

--- js/modeshift_nav.js
+++ js/modeshift_nav.js
@@ -1,3 +1,33 @@
 $(function(){
     $("#n52_211").load("modeshift_nav.html");
   });
+
+
+
+submit();
+
+function submit(){
+    $('#n52_188').click(function(){
+        var status=true;
+        $('.required').each(function(){
+
+            var element=$(this);
+            var elementVal=$(this).val();
+            var errorMsgId=element.attr('data-errorMsg');
+            if(elementVal==''){
+                $('.'+errorMsgId).show();
+                element.addClass('errorField');
+                status=false;
+            }
+            else{
+                $('.'+errorMsgId).hide();
+                element.removeClass('errorField');
+                }
+        });
+
+        if(status) {
+            $('.alert-success').show();
+        }
+
+    });
+}
